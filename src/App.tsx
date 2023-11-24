e184ad3ed06c07c76cad3417942f3349989b7a78
Ian Torquato <iantorquato2@gmail.com>
2023-11-24 19:58:20 -0300

    Add Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-function App() {
+import { Home } from "./pages/Home";
+
+export function App() {
   return (
     <div>
-      <h1>App</h1>
+      <Home />
     </div>
   )
 }
-
-export default App
