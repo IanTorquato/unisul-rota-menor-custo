3c8c996b8974d6756b39c4adaddf4d2e14fa1905
Ian Torquato <iantorquato2@gmail.com>
2023-11-24 20:18:36 -0300

    Add css reset

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import ptBr from 'antd/lib/locale/pt_BR';
 
 import { Home } from "./pages/Home";
 
+import './styles/global.scss';
+
 export function App() {
   return (
     <ConfigProvider locale={ptBr}>
