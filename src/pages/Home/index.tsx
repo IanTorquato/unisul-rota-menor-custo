611a3fad79659898ecf48ed6562a04fbd1d20c0e
Ian Torquato <iantorquato2@gmail.com>
2023-11-24 22:28:30 -0300

    Add mocked map

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,31 @@
-import { Typography } from 'antd';
+import { Card, Flex, Layout, Typography } from 'antd';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
+import './styles.scss';
 
 export function Home() {
   return (
-    <div>
-      <Typography.Title>Home</Typography.Title>
+    <div className="home-container">
+      <Flex className="header" justify="center">
+        <Typography.Title level={1}>Centro Ocidental Paranaense</Typography.Title>
+      </Flex>
+
+      <Layout.Content className="map-container">
+        <Card size="small">
+          <MapContainer center={[-28.3341378, -49.0332305]} id="map" scrollWheelZoom={false} zoom={14}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={[-28.3301378, -49.0322305]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </Card>
+      </Layout.Content>
     </div>
   );
 }
