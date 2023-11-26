588b6b29bdc78c117aba9db12d3cb114941e5ef8
Ian Torquato <iantorquato2@gmail.com>
2023-11-26 17:10:27 -0300

    Improve city selects

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,20 +7,35 @@ import { getColBreakpoints } from 'src/utils/getColBreakpoints';
 import './styles.scss';
 
 const CITY_OPTIONS: DefaultOptionType[] = [
-  { label: 'Ortigueira', value: 'Ortigueira' },
-  { label: 'Imbaú', value: 'Imbaú' },
-  { label: 'Reserva', value: 'Reserva' },
-  { label: 'Tibagi', value: 'Tibagi' },
-  { label: 'Telêmaco Borba', value: 'Telêmaco Borba' },
-  { label: 'Ventania', value: 'Ventania' },
-  { label: 'Arapoti', value: 'Arapoti' },
-  { label: 'Sengés', value: 'Sengés' },
-  { label: 'Jaguariaíva', value: 'Jaguariaíva' },
-  { label: 'Piraí do Sul', value: 'Piraí do Sul' },
-  { label: 'Castro', value: 'Castro' },
-  { label: 'Carambeí', value: 'Carambeí' },
-  { label: 'Palmeira', value: 'Palmeira' },
-  { label: 'Ponta Grossa', value: 'Ponta Grossa' },
+  {
+    label: 'Microrregião Telêmaco Borda',
+    options: [
+      { label: 'Ortigueira', value: 'Ortigueira' },
+      { label: 'Imbaú', value: 'Imbaú' },
+      { label: 'Reserva', value: 'Reserva' },
+      { label: 'Tibagi', value: 'Tibagi' },
+      { label: 'Telêmaco Borba', value: 'Telêmaco Borba' },
+      { label: 'Ventania', value: 'Ventania' },
+    ],
+  },
+  {
+    label: 'Microrregião Jaguariaíva',
+    options: [
+      { label: 'Arapoti', value: 'Arapoti' },
+      { label: 'Sengés', value: 'Sengés' },
+      { label: 'Jaguariaíva', value: 'Jaguariaíva' },
+      { label: 'Piraí do Sul', value: 'Piraí do Sul' },
+    ],
+  },
+  {
+    label: 'Microrregião Ponta Grossa',
+    options: [
+      { label: 'Castro', value: 'Castro' },
+      { label: 'Carambeí', value: 'Carambeí' },
+      { label: 'Palmeira', value: 'Palmeira' },
+      { label: 'Ponta Grossa', value: 'Ponta Grossa' },
+    ],
+  },
 ];
 
 export function Home() {
