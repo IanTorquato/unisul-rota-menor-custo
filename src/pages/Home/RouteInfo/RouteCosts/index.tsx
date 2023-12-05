2904990c92705dcfa8198c71091bdcdacd5cb3f9
Ian Torquato <iantorquato2@gmail.com>
2023-12-05 20:56:55 -0300

    Improve `RouteInfo`

--- src/pages/Home/RouteInfo/RouteCosts/index.tsx
+++ src/pages/Home/RouteInfo/RouteCosts/index.tsx
@@ -1,14 +1,17 @@
 import { faGasPump, faRoadBarrier, faUtensils } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Flex, Space, Typography } from 'antd';
+
 import './styles.scss';
 
 export function RouteCosts() {
   return (
     <Flex align="center" className="route-costs-container" vertical>
       <Space size={12}>
-        <Typography.Text>Custos estimados:</Typography.Text>
-        <Typography.Text type="warning">R$ 400,00</Typography.Text>
+        <Typography.Text strong>Custos estimados:</Typography.Text>
+        <Typography.Text type="warning" strong>
+          R$ 400,00
+        </Typography.Text>
       </Space>
 
       <Flex justify="space-evenly" style={{ width: '100%' }}>
