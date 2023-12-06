36e9c1f02b661ac6c339063ff186e1d1ac36fe05
Ian Torquato <iantorquato2@gmail.com>
2023-12-05 22:34:33 -0300

    Improve `RouteInfo`

--- src/pages/Home/RouteInfo/RouteCosts/index.tsx
+++ src/pages/Home/RouteInfo/RouteCosts/index.tsx
@@ -29,21 +29,23 @@ export function RouteCosts({ selectedVehicle }: RouteCostsProps) {
     <Flex align="center" className="route-costs-container" vertical>
       <Space size={12}>
         <Typography.Text strong>Custos estimados:</Typography.Text>
-        <Typography.Text type="warning" strong>
+
+        <Typography.Text type="success" strong>
+          {/* TODO: Calculate total */}
           R$ 400,00
         </Typography.Text>
       </Space>
 
       <Flex justify="space-evenly" style={{ width: '100%' }}>
-        <Tooltip placement="bottom" title="Pedágio">
-          <Space size={4}>
-            <FontAwesomeIcon icon={faRoadBarrier} /> <div>R$ 150,00</div>
-          </Space>
-        </Tooltip>
-
         <Tooltip placement="bottom" title="Combustível">
           <Space size={4}>
             <FontAwesomeIcon icon={faGasPump} /> <div>{calculateCost()}</div>
+          </Space>
+        </Tooltip>
+
+        <Tooltip placement="bottom" title="Pedágio">
+          <Space size={4}>
+            <FontAwesomeIcon icon={faRoadBarrier} /> <div>R$ 150,00</div>
           </Space>
         </Tooltip>
 
